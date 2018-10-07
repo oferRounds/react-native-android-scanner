83b041b96714104ebf5a6ba186e199fecccfe7b8
ofer morag <ofer.morag@hibob.io>
2018-10-07 15:47:52 +0300

    Fixed build failure

--- android/src/main/java/com/reactlibrary/RNReactNativeAndroidScannerModule.java
+++ android/src/main/java/com/reactlibrary/RNReactNativeAndroidScannerModule.java
@@ -8,7 +8,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
